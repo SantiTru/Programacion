0a725c156d563c157a35b8088f49a79616833ac6
SantiTru <santiagotrujillonavarro@gmail.com>
2023-03-26 01:07:27 +0100

    commit

--- EjerciciosDelLibro/Tema8/C8_1/Funciones/misFunciones/Geometria.java
+++ EjerciciosDelLibro/Tema8/C8_1/Funciones/misFunciones/Geometria.java
@@ -38,8 +38,64 @@ public class Geometria {
         teclado.close(); 
         return radio;
     }
-    
-  
-
-  
+/**
+*  @info: función para calcular el perímetro de un rectángulo
+*  @param:
+*    - "ancho": ancho del rectángulo
+*    - "alto": alto del rectángulo
+*  @return perimetro: perímetro del rectángulo
+*/
+    public static double perimetroRectangulo(double ancho, double alto){
+//variable para el perímetro
+        double perimetro=(ancho*=2)+(alto*=2);
+        return perimetro;
+    }
+/**
+*  @info: función para calcular el área de un rectángulo
+*  @param:
+*    - "ancho": ancho del rectángulo
+*    - "alto": alto del rectángulo
+*  @return area: área del rectángulo
+*/
+    public static double areaRectangulo(double ancho, double alto){
+//variable para el perímetro
+        double area=ancho*alto; 
+        return area;
+    }  
+/**
+*  @info: función para calcular la circunferencia dado un radio
+*  @param radio: radio introducido
+*  @return circunferencia: circunferencia del radio
+*/
+    public static double circunferencia(double radio){
+//constantes para pi
+        final double pi=3.1416;   
+//cálculo de la circunferencia
+        double circunferencia=2*pi*radio;   
+        return circunferencia;
+    }
+/**
+*  @info: función para calcular el área de un circulo dado un radio
+*  @param radio: radio introducido
+*  @return area: área del circulo
+*/
+    public static double areaCirculo(double radio){
+//constantes para pi
+        final double pi=3.1416;   
+//cálculo del área
+        double area=pi*Math.pow(radio, 2);  
+        return area;
+    }
+/**
+*  @info: función para calcular el volumen de una esfera dado un radio
+*  @param radio: radio introducido
+*  @return volumen: volumen de la esfera
+*/
+    public static double volumenCirculo(double radio){
+//constantes para pi
+        final double pi=3.1416;   
+//cálculo del área
+        double volumen=(4/3)*pi*Math.pow(radio, 3); 
+        return volumen;
+    }
 }
