5fe2eb93b6c3554d0c3592c777ed369f89bc9497
SantiTru <santiagotrujillonavarro@gmail.com>
2023-03-28 07:47:36 +0200

    commit

--- EjerciciosDelLibro/Tema7/C7_1/Ejer6Tema7.java
+++ EjerciciosDelLibro/Tema7/C7_1/Ejer6Tema7.java
@@ -1,5 +1,39 @@
 package EjerciciosDelLibro.Tema7.C7_1;
+import java.util.Scanner;
 
-public class Ejer6Tema7 {
-  
+/**
+*  @author: SantiTru
+*  @file: Ejer6Tema7.java
+*  @info: Escribe un programa que lea 15 números por teclado y que los almacene en un
+*         array. Rota los elementos de ese array, es decir, el elemento de la posición 0
+*         debe pasar a la posición 1, el de la 1 a la 2, etc. El número que se encuentra en
+*         la última posición debe pasar a la posición 0. Finalmente, muestra el contenido
+*         del array.
+*/
+
+public class Ejer6Tema7{
+  public static void main(String[] args){
+// creación array
+        int[] n=new int[15];
+// asignación de valores
+// apertura de scanner
+          Scanner s=new Scanner(System.in);
+// obtención de los valores
+          for(int i=0; i<15; i++){
+            System.out.print("Dime un número: ");
+              n[i]=s.nextInt();
+          }
+// cierre de scanner
+          s.close();
+// rotación y variable auxiliar
+        int aux=n[14];
+        for(int i=14; i>0; i--){
+          n[i]=n[i-1];
+        }
+        n[0]=aux;
+// contenido final
+      for(int i=0; i<15; i++){
+        System.out.print("| "+n[i]+" |");
+      }
+  }
 }
--- EjerciciosDelLibro/Tema7/C7_2/Bidimensionales/Ejer1BiTema7.java
+++ EjerciciosDelLibro/Tema7/C7_2/Bidimensionales/Ejer1BiTema7.java
@@ -1,5 +1,49 @@
 package EjerciciosDelLibro.Tema7.C7_2.Bidimensionales;
 
-public class Ejer1BiTema7 {
-  
+/**
+*  @author: SantiTru
+*  @file: Ejer1BiTema7.java
+*  @info: Define un array de números enteros de 3 filas por 6 columnas con nombre num
+*         y asigna los valores según la siguiente tabla. Muestra el contenido de todos
+*         los elementos del array dispuestos en forma de tabla como se muestra en la
+*         figura.
+*/
+
+public class Ejer1BiTema7{
+  public static void main(String[] args){
+// creación del array
+// nº elementos
+// filas
+          int f=3;
+          int fila;
+// columnas
+          int c=6;
+          int columna;
+// creación
+        int[][] array=new int[f][c];
+// asignación de valores
+// fila 0
+        array[0][0]=0;
+        array[0][1]=30;
+        array[0][2]=2;
+        array[0][5]=5;
+// fila 1
+        array[1][0]=75;
+        array[1][4]=0;
+ // fila 2
+        array[2][2]=-2;
+        array[2][3]=9;
+        array[2][5]=11;
+// contenido del array
+      System.out.println("+----+----+----+----+----+----+");
+
+      for(fila=0; fila<f; fila++){
+        for(columna=0; columna<c; columna++){
+          System.out.printf("| %-2d ", array[fila][columna]);
+        }
+        System.out.println("|");
+        System.out.print("+----+----+----+----+----+----+");
+        System.out.println();
+      }
+  }
 }
