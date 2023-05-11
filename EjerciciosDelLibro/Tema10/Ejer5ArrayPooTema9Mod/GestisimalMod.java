1d9ef92a51c1e435546c7791e566a6d1166e6378
SantiTru <santiagotrujillonavarro@gmail.com>
2023-05-12 00:38:24 +0200

    commit

--- EjerciciosDelLibro/Tema10/Ejer5ArrayPooTema9Mod/GestisimalMod.java
+++ EjerciciosDelLibro/Tema10/Ejer5ArrayPooTema9Mod/GestisimalMod.java
@@ -1,40 +1,27 @@
 package EjerciciosDelLibro.Tema10.Ejer5ArrayPooTema9Mod;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Scanner;
-
-/**
- * @author: SantiTru
- * @file: GestisimalMod.java
- * @info: programa principal del Ejercicio 5 del Tema 9. Libro: Aprende Java con ejercicios.
- */
 
 public class GestisimalMod {
 
-//N determina el tamaño del array
-  static final int N = 100;
-
-  static ArticuloMod[] articulos = new ArticuloMod[N];
-  //ArrayList<ArticuloMod> articulosLista = new ArrayList<ArticuloMod>();
+  // ArrayList que contiene todos los artículos
+  static ArrayList<ArticuloMod> articulos = new ArrayList<ArticuloMod>();
 
   public static void main(String[] args) {
 
-//Inicializa el array
-    for (int i = 0; i < N; i++) {
-      GestisimalMod.articulos[i] = new ArticuloMod();
-    }
-//Definimos las variables 
+    // Definimos las variables
     int opcion;
-    int stockIntroducido;
+    String codigoIntroducido;
+    String nombreIntroducido;
+    String familiaIntroducida;
     double precioDeCompraIntroducido;
     double precioDeVentaIntroducido;
-    String codigoIntroducido = "";
-    String nombreIntroducido = "";
-    String familiaIntroducida = "";
+    int stockIntroducido;
 
     Scanner teclado = new Scanner(System.in);
 
-//Definimos el menú
+    // Definimos el menú
     do {
       pintaTitulo("G E S T I S I M A L");
       System.out.println("1. Listado");
@@ -50,137 +37,146 @@ public class GestisimalMod {
 
       switch (opcion) {
 
-//Opción del menú "Listado"
+        // Opción del menú "Listado"
         case 1:
           pintaTitulo("LISTADO");
 
           for (ArticuloMod a : articulos) {
-            if (!a.getCodigo().equals("LIBRE")) {
-              System.out.println(a);
-            }
+            System.out.println(a);
           }
 
           break;
 
-//Opción del menú "Alta"
+        // Opción del menú "Alta"
         case 2:
           pintaTitulo("NUEVO ARTÍCULO");
 
-          if (primeraPosicionLibre() == N) {
-            System.out.println("La base de datos está llena.");
-          } else {
+          // Introducción de datos
+          System.out.println("Introduzca los datos del artículo.");
+          System.out.print("Código: ");
 
-//Introducción de datos
-            System.out.println("Introduzce los datos del artículo.");
-            System.out.print("Código: ");
+          // Comprueba que el código introducido no se repita
+          do {
+            codigoIntroducido = teclado.nextLine();
+            if (existeCodigo(codigoIntroducido)) {
+              System.out.println("El código ya existe en la base de datos.");
+              System.out.print("Introduzca otro código: ");
+            }
+          } while (existeCodigo(codigoIntroducido));
 
-//Comprueba que el código introducido no se repita
-            do {
-              codigoIntroducido = teclado.nextLine();
-              if (existeCodigo(codigoIntroducido)) {
-                System.out.println("El código ya existe en la base de datos.");
-                System.out.print("Introduzce otro código: ");
-              }
-            } while (existeCodigo(codigoIntroducido));
+          System.out.print("Nombre del producto: ");
+          nombreIntroducido = teclado.nextLine();
+          System.out.print("Familia del producto: ");
+          familiaIntroducida = teclado.nextLine();
+          System.out.print("Precio de compra: ");
+          precioDeCompraIntroducido = Double.parseDouble(teclado.nextLine());
+          System.out.print("Precio de venta: ");
+          precioDeVentaIntroducido = Double.parseDouble(teclado.nextLine());
+          System.out.print("Stock: ");
+          stockIntroducido = Integer.parseInt(teclado.nextLine());
 
-            System.out.print("Nombre del producto: ");
-            nombreIntroducido = teclado.nextLine();
-            System.out.print("Familia del producto: ");
-            familiaIntroducida = teclado.nextLine();
-            System.out.print("Precio de compra: ");
-            precioDeCompraIntroducido = Double.parseDouble(teclado.nextLine());
-            System.out.print("Precio de venta: ");
-            precioDeVentaIntroducido = Double.parseDouble(teclado.nextLine());
-            System.out.print("Stock: ");
-            stockIntroducido = Integer.parseInt(teclado.nextLine());
-
-//Crea el nuevo artículo
-            articulos[primeraPosicionLibre()] = new ArticuloMod(
-                codigoIntroducido, nombreIntroducido, familiaIntroducida, precioDeCompraIntroducido,
-                precioDeVentaIntroducido, stockIntroducido);
-          }
+          // Crea el nuevo artículo y lo agrega a la lista de artículos
+          ArticuloMod nuevoArticulo = new ArticuloMod();
+          nuevoArticulo.setCodigo(codigoIntroducido);
+          nuevoArticulo.setNombreProducto(nombreIntroducido);
+          nuevoArticulo.setFamilia(familiaIntroducida);
+          nuevoArticulo.setPrecioDeCompra(precioDeCompraIntroducido);
+          nuevoArticulo.setPrecioDeVenta(precioDeVentaIntroducido);
+          nuevoArticulo.setStock(stockIntroducido);
+          articulos.add(nuevoArticulo);
 
           break;
 
-//Opción del menú "Baja"
+        // Opción del menú "Baja"
         case 3:
           pintaTitulo("BAJA");
 
-          System.out.print("Introduzce el código del artículo: ");
+          System.out.print("Introduzca el código del artículo: ");
           codigoIntroducido = teclado.nextLine();
 
-          if (!existeCodigo(codigoIntroducido)) {
+          boolean encontrado = false;
+          for (int i = 0; i < articulos.size(); i++) {
+            if (articulos.get(i).getCodigo().equals(codigoIntroducido)) {
+              articulos.remove(i);
+              System.out.println("Artículo borrado.");
+              encontrado = true;
+              break;
+            }
+          }
+          if (!encontrado) {
             System.out.println("El código introducido no existe.");
-          } else {
-            articulos[posicionConCodigo(codigoIntroducido)].setCodigo("LIBRE");
-            System.out.println("Artículo borrado.");
           }
 
           break;
 
-//Opción del menú "Modificación"
+        // Opción del menú "Modificación"
         case 4:
           pintaTitulo("MODIFICACIÓN");
 
-          System.out.print("Introduzce el código del artículo: ");
+          System.out.print("Introduzca el código del artículo: ");
           codigoIntroducido = teclado.nextLine();
 
           if (!existeCodigo(codigoIntroducido)) {
             System.out.println("El código introducido no existe.");
           } else {
-            System.out.print("Introduzce los nuevos datos del artículo");
-            System.out.println(" o INTRO para dejarlos igual.");
-
             int i = posicionConCodigo(codigoIntroducido);
 
-            System.out.println("Código: " + articulos[i].getCodigo());
-            System.out.print("Nuevo código: ");
-            codigoIntroducido = teclado.nextLine();
-            if (!codigoIntroducido.equals("")) {
-              articulos[i].setCodigo(codigoIntroducido);
+            System.out.print("Nuevo código (" + articulos.get(i).getCodigo() + "): ");
+            String nuevoCodigo = teclado.nextLine();
+            if (!nuevoCodigo.equals("")) {
+              // Modifica el código del artículo
+              ArticuloMod articuloModificado = articulos.get(i);
+              articuloModificado.setCodigo(nuevoCodigo);
             }
 
-            System.out.println("Nombre del producto: " + articulos[i].getNombreProducto());
-            System.out.print("Nuevo nombre: ");
+            System.out.print("Nuevo nombre (" + articulos.get(i).getNombreProducto() + "): ");
             nombreIntroducido = teclado.nextLine();
             if (!nombreIntroducido.equals("")) {
-              articulos[i].setNombreProducto(nombreIntroducido);
+              // Modifica el nombre del artículo
+              ArticuloMod articuloModificado = articulos.get(i);
+              articuloModificado.setNombreProducto(nombreIntroducido);
             }
 
-            System.out.println("Familia del producto: " + articulos[i].getFamilia());
-            System.out.print("Nueva familia: ");
+            System.out.print("Nueva familia (" + articulos.get(i).getFamilia() + "): ");
             familiaIntroducida = teclado.nextLine();
             if (!familiaIntroducida.equals("")) {
-              articulos[i].setFamilia(familiaIntroducida);
+              // Modifica la familia del artículo
+              ArticuloMod articuloModificado = articulos.get(i);
+              articuloModificado.setFamilia(familiaIntroducida);
             }
 
-            System.out.println("Precio de compra: " + articulos[i].getPrecioDeCompra());
-            System.out.print("Nuevo precio de compra: ");
-            String precioDeCompraIntroducidoString = teclado.nextLine();
-            if (!precioDeCompraIntroducidoString.equals("")) {
-              articulos[i].setPrecioDeCompra(Double.parseDouble(precioDeCompraIntroducidoString));
+            System.out.print("Nuevo precio de compra (" + articulos.get(i).getPrecioDeCompra() + "): ");
+            String nuevoPrecioCompra = teclado.nextLine();
+            if (!nuevoPrecioCompra.equals("")) {
+              // Modifica el precio de compra del artículo
+              ArticuloMod articuloModificado = articulos.get(i);
+              articuloModificado.setPrecioDeCompra(Double.parseDouble(nuevoPrecioCompra));
             }
-            System.out.println("Precio de venta: " + articulos[i].getPrecioDeVenta());
-            System.out.print("Nuevo precio de venta: ");
-            String precioDeVentaIntroducidoString = teclado.nextLine();
-            if (!precioDeVentaIntroducidoString.equals("")) {
-              articulos[i].setPrecioDeVenta(Double.parseDouble(precioDeVentaIntroducidoString));
+
+            System.out.print("Nuevo precio de venta (" + articulos.get(i).getPrecioDeVenta() + "): ");
+            String nuevoPrecioVenta = teclado.nextLine();
+            if (!nuevoPrecioVenta.equals("")) {
+              // Modifica el precio de venta del artículo
+              ArticuloMod articuloModificado = articulos.get(i);
+              articuloModificado.setPrecioDeVenta(Double.parseDouble(nuevoPrecioVenta));
             }
-            System.out.println("Stock: " + articulos[i].getStock());
-            System.out.print("Nuevo stock: ");
-            String stockIntroducidoString = teclado.nextLine();
-            if (!stockIntroducidoString.equals("")) {
-              articulos[i].setStock(Integer.parseInt(stockIntroducidoString));
+
+            System.out.print("Nuevo stock (" + articulos.get(i).getStock() + "): ");
+            String nuevoStock = teclado.nextLine();
+            if (!nuevoStock.equals("")) {
+              // Modifica el stock del artículo
+              ArticuloMod articuloModificado = articulos.get(i);
+              articuloModificado.setStock(Integer.parseInt(nuevoStock));
             }
           }
 
           break;
 
-//Opción del menú "Entrada de mercancía"
+        // Opción del menú "Entrada de mercancía"
         case 5:
           pintaTitulo("ENTRADA DE MERCANCÍA");
 
-          System.out.print("Introduzce el código del artículo: ");
+          System.out.print("Introduzca el código del artículo: ");
           codigoIntroducido = teclado.nextLine();
 
           if (!existeCodigo(codigoIntroducido)) {
@@ -188,21 +184,28 @@ public class GestisimalMod {
           } else {
             int i = posicionConCodigo(codigoIntroducido);
             System.out.println("Entrada de mercancía del siguiente artículo: ");
-            System.out.println(articulos[i]);
-            System.out.print("Introduzce el número de unidades que entran: ");
+            System.out.println(articulos.get(i));
+            System.out.print("Introduzca el número de unidades que entran: ");
             String stockIntroducidoString = teclado.nextLine();
-            articulos[i].setStock(
-                Integer.parseInt(stockIntroducidoString) + articulos[i].getStock());
-            System.out.println("La mercancía ha entrado en el almacén.");
+            if (!stockIntroducidoString.equals("")) {
+              // Añade la cantidad de stock introducida al artículo
+              ArticuloMod articuloModificado = articulos.get(i);
+              int stockAnterior = articuloModificado.getStock();
+              int stockEntrante = Integer.parseInt(stockIntroducidoString);
+              articuloModificado.setStock(stockAnterior + stockEntrante);
+              System.out.println("La mercancía ha entrado en el almacén.");
+            } else {
+              System.out.println("Entrada cancelada.");
+            }
           }
 
           break;
 
-//Opción del menú "Salida de mercancia"
+        // Opción del menú "Salida de mercancia"
         case 6:
           pintaTitulo("SALIDA DE MERCANCÍA");
 
-          System.out.print("Introduzce el código del artículo: ");
+          System.out.print("Introduzca el código del artículo: ");
           codigoIntroducido = teclado.nextLine();
 
           if (!existeCodigo(codigoIntroducido)) {
@@ -210,47 +213,42 @@ public class GestisimalMod {
           } else {
             int i = posicionConCodigo(codigoIntroducido);
             System.out.println("Salida de mercancía del siguiente artículo: ");
-            System.out.println(articulos[i]);
-            System.out.print("Introduzce el número de unidades que desea sacar del almacén: ");
+            System.out.println(articulos.get(i));
+            System.out.print("Introduzca el número de unidades que desea sacar del almacén: ");
             stockIntroducido = Integer.parseInt(teclado.nextLine());
-            if (articulos[i].getStock() - stockIntroducido > 0) {
-              articulos[i].setStock(articulos[i].getStock() - stockIntroducido);
+            if (stockIntroducido == 0) {
+              System.out.println("Operación cancelada.");
+            } else if (stockIntroducido < 0) {
+              System.out.println("Operación incorreca.");
+            } else if (articulos.get(i).getStock() - stockIntroducido >= 0) {
+              // Resta la cantidad de stock introducida al artículo
+              ArticuloMod articuloModificado = articulos.get(i);
+              int stockAnterior = articuloModificado.getStock();
+              articuloModificado.setStock(stockAnterior - stockIntroducido);
               System.out.println("La mercancía ha salido del almacén.");
             } else {
-              System.out.println("Lo siento, no hay tanto producto en el almacén.");
+              System.out.println("No hay suficiente mercancía en el almacén.");
             }
           }
 
           break;
-//Opción del mené "Borrar todo el stock"
-          case 7:
+
+        // Opción del mené "Borrar todo el stock"
+        case 7:
           pintaTitulo("BORRADO DE STOCK");
           borrarStock();
           System.out.println("Stock borrado.");
-      } //Cerramos el switch
+          break;
+      } // Cerramos el switch
     } while (opcion != 8);
     System.out.println("\u001B[31mGracias por utilizar nuestro programa. ¡Hasta Luego Lucas!\033[0m");
     teclado.close();
   }
 
-//Funciones necesarias
+  // Funciones necesarias
 
   /**
-   * @info: Busca la primera posición libre del array. Si no quedan huecos, devuelve -1.
-   * 
-   * @return primera posición libre del array o -1 si no quedan huecos
-   */
-  public static int primeraPosicionLibre() {
-    for (int i = 0; i < articulos.length; i++) {
-      if (articulos[i].getCodigo().equals("LIBRE")) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
-  /**
-   * @info: Dice si existe o no un artículo con un determinado código.
+   * @info: Dice si existe o no un artículo con un determinado código. *
    * 
    * @return true si existe el código y false si no existe
    */
@@ -275,30 +273,29 @@ public class GestisimalMod {
   }
 
   /**
-   * @info:Devuelve la posición dentro del array en la que se encuentra un artículo
-   *       con un determinado código. Si el código no se encuentra, devuelve -1.
+   * @info:Devuelve la posición dentro del array en la que se encuentra un
+   *                artículo con un determinado código. Si el código no se
+   *                encuentra,
+   *                devuelve -1. *
    * 
    * @return posición dentro del array en la que se encuentra un artículo
    */
   public static int posicionConCodigo(String codigo) {
-    for (int i = 0; i < articulos.length; i++) {
-      if (articulos[i].getCodigo().equals(codigo)) {
+    for (int i = 0; i < articulos.size(); i++) {
+      if (articulos.get(i).getCodigo().equals(codigo)) {
         return i;
       }
     }
     return -1;
   }
+
   /**
    * @info: Borra todo el contenido del stock del array
    */
   public static void borrarStock() {
     for (ArticuloMod borra : articulos) {
-        if (!borra.getCodigo().equals("LIBRE")) {
-          borra.setStock(0);
-        }
+      borra.setStock(0);
     }
-    System.out.println("Stock de todos los artículos borrado.");
   }
-
 
 }
